9096b7a39a0c53e6bb7b7960d819908efa1ccd55
moontai0724 <moontai0724@gmail.com>
2022-01-27 12:03:13 +0800

    fix: change order of scripts

---
+++ SCRIPT_HEADER.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         巴哈動漫電玩通題庫與解答系統
+// @namespace    https://home.gamer.com.tw/moontai0724
+// @version      5.0.0
+// @description  巴哈動漫電玩通題庫與解答系統，蒐集題庫中～
+// @author       moontai0724
+// @match        https://forum.gamer.com.tw/B.php*
+// @supportURL   https://home.gamer.com.tw/creationDetail.php?sn=3924920
+// @grant        GM_getResourceText
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @connect      script.google.com
+// @connect      script.googleusercontent.com
+// @resource     quizrp https://raw.githubusercontent.com/moontai0724/bahamut-quiz-script/master/right-box.html
+// @license      MIT
+// ==/UserScript==
